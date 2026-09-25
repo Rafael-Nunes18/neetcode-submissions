877054b2b3129d7e18aa6b97be7df99902bfd0dc
Rafael Toledo Nunes <rafael.nunes1724@gmail.com>
2026-09-25 15:56:33 -0300

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -0,0 +1,13 @@
+//Brutal force solution O(n^2)
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    for(int i = 0; i<nums.Length; i++){
+        for(int j = i+1; j<nums.Length; j++){
+           if(nums[i] + nums[j] == target)
+           return new int[] {i, j};
+        }
+    }
+    return new int[]{};
+    }
+}
